8486fdb854eb5ac285a5cf4b1618e361dbaaa79d
KayJuno <dangdan2807@gmail.com>
2022-11-24 22:39:51 +0700

    refactor

--- api-server/src/util/generate.util.js
+++
@@ -1,9 +0,0 @@
-const shortid = require('shortid');
-
-/**
- * Tạo 1 id bằng cách lấy thời gian lúc gọi hàm (dạng số) + 1 short id
- * @returns Trả về 1 chuỗi id
- */
-const generateId = () => `${new Date().getTime()}${shortid.generate()}`;
-
-module.exports = { generateId };
